73c0f0982492f87001f0876ed1cb03c97e965abe
jingyang022 <jingyang022@yahoo.com.sg>
2025-03-17 15:29:07 +0800

    updated files

--- bucket.tf
+++ bucket.tf
@@ -8,7 +8,7 @@ resource "aws_s3_bucket_notification" "bucket_notification" {
   lambda_function {
     lambda_function_arn = aws_lambda_function.func.arn
     events              = ["s3:ObjectCreated:*"]
-    filter_prefix       = "uploads/"
+    #filter_prefix       = "uploads/"
     #filter_suffix       = ".log"
   }
 

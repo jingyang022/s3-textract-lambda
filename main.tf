99b3b7c9580a5de326f658f4187a1544e2433a40
jingyang022 <jingyang022@yahoo.com.sg>
2025-03-25 13:31:14 +0800

    updated files

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 # Define an archive_file datasource that creates the lambda archive
 data "archive_file" "lambda" {
  type        = "zip"
- source_file = "hello.py"
- output_path = "hello.zip"
+ source_file = "process-textract.py"
+ output_path = "process-textract.zip"
 }
 
 resource "aws_lambda_permission" "allow_bucket" {
@@ -14,15 +14,15 @@ resource "aws_lambda_permission" "allow_bucket" {
 }
 
 resource "aws_lambda_function" "func" {
- function_name = "yap-process-s3events"
+ function_name = "textract-process-func"
  role          = aws_iam_role.lambda_exec_role.arn
- handler       = "hello.lambda_handler"
+ handler       = "process-textract.lambda_handler"
  runtime       = "python3.13"
  filename      = data.archive_file.lambda.output_path
 }
 
 # aws_cloudwatch_log_group to get the logs of the Lambda execution.
 resource "aws_cloudwatch_log_group" "lambda_log_group" {
- name              = "/aws/lambda/yap-process-s3events"
+ name              = "/aws/lambda/textract-process-func"
  retention_in_days = 14
 }
